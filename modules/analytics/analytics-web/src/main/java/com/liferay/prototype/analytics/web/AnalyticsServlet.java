8da5d185dc7ab8306ad76fc0697cdcec3869c475
Michael Han <michael.han@liferay.com>
2017-03-29 13:12:01 -0500

    sourceformat

--- modules/analytics/analytics-web/src/main/java/com/liferay/prototype/analytics/web/AnalyticsServlet.java
+++ modules/analytics/analytics-web/src/main/java/com/liferay/prototype/analytics/web/AnalyticsServlet.java
@@ -9,84 +9,34 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.prototype.analytics.processor.AnalyticsMessageProcessor;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author Michael C. Han
  */
 @Component(
 	immediate = true,
-	property = {
-		"osgi.http.whiteboard.servlet.pattern=/analytics",
-	},
+	property = {"osgi.http.whiteboard.servlet.pattern=/analytics"},
 	service = Servlet.class
 )
 public class AnalyticsServlet extends HttpServlet {
-
-	@Override
-	protected void service(
-			HttpServletRequest httpServletRequest,
-			HttpServletResponse httpServletResponse)
-		throws ServletException, IOException {
-
-		StringBundler sb = new StringBundler();
-
-		String value = null;
-
-		BufferedReader reader = httpServletRequest.getReader();
-
-		while ((value = reader.readLine()) != null) {
-			sb.append(value);
-		}
-
-		String analyticsMessage = sb.toString();
-		try {
-			JSONObject jsonObject = _jsonFactory.createJSONObject(
-				analyticsMessage);
-
-			String messageFormat = jsonObject.getString("messageFormat");
-
-			if (Validator.isNull(messageFormat)) {
-				if (_log.isWarnEnabled()) {
-					_log.warn("No message format: " + analyticsMessage);
-				}
-
-				return;
-			}
-
-			AnalyticsMessageProcessor analyticsMessageProcessor =
-				_analyticsMessageProcessors.get(messageFormat);
-
-			if (analyticsMessageProcessor == null) {
-				if (_log.isWarnEnabled()) {
-					_log.warn("No message processor for : " + messageFormat);
-				}
-
-				return;
-			}
-
-			analyticsMessageProcessor.processMessage(jsonObject);
-		}
-		catch (JSONException e) {
-			if (_log.isWarnEnabled()) {
-				_log.warn("Could not parse: " + analyticsMessage, e);
-			}
-		}
-	}
 
 	@Reference(
 		cardinality = ReferenceCardinality.AT_LEAST_ONE,
@@ -115,13 +65,65 @@ public class AnalyticsServlet extends HttpServlet {
 		_analyticsMessageProcessors.remove(messageFormat);
 	}
 
+	@Override
+	protected void service(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException, ServletException {
+
+		StringBundler sb = new StringBundler();
+
+		String value = null;
+
+		BufferedReader reader = httpServletRequest.getReader();
+
+		while ((value = reader.readLine()) != null) {
+			sb.append(value);
+		}
+
+		String analyticsMessage = sb.toString();
+
+		try {
+			JSONObject jsonObject = _jsonFactory.createJSONObject(
+				analyticsMessage);
+
+			String messageFormat = jsonObject.getString("messageFormat");
+
+			if (Validator.isNull(messageFormat)) {
+				if (_log.isWarnEnabled()) {
+					_log.warn("No message format: " + analyticsMessage);
+				}
+
+				return;
+			}
+
+			AnalyticsMessageProcessor analyticsMessageProcessor =
+				_analyticsMessageProcessors.get(messageFormat);
+
+			if (analyticsMessageProcessor == null) {
+				if (_log.isWarnEnabled()) {
+					_log.warn("No message processor for : " + messageFormat);
+				}
+
+				return;
+			}
+
+			analyticsMessageProcessor.processMessage(jsonObject);
+		}
+		catch (JSONException jsone) {
+			if (_log.isWarnEnabled()) {
+				_log.warn("Could not parse: " + analyticsMessage, jsone);
+			}
+		}
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		AnalyticsServlet.class);
 
-	@Reference
-	private JSONFactory _jsonFactory;
-
 	private Map<String, AnalyticsMessageProcessor> _analyticsMessageProcessors =
 		new ConcurrentHashMap<>();
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 }
