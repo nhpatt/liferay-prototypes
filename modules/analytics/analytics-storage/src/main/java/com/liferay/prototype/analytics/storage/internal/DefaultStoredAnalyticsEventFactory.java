bd61857f21e3df9a38bf68335dcf1d8c80097ced
Michael Han <michael.han@liferay.com>
2017-03-30 12:57:36 -0500

    implement conversion between message sent from event generators to that being indexed

--- modules/analytics/analytics-storage/src/main/java/com/liferay/prototype/analytics/storage/internal/DefaultStoredAnalyticsEventFactory.java
+++ modules/analytics/analytics-storage/src/main/java/com/liferay/prototype/analytics/storage/internal/DefaultStoredAnalyticsEventFactory.java
@@ -1,10 +1,20 @@
 package com.liferay.prototype.analytics.storage.internal;
 
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.prototype.analytics.data.binding.stubs.AnalyticsEvents;
+import com.liferay.prototype.analytics.data.binding.stubs.Event;
 import com.liferay.prototype.analytics.storage.StoredAnalyticsEventFactory;
+import com.liferay.prototype.analytics.storage.stubs.AdditionalInfo;
+import com.liferay.prototype.analytics.storage.stubs.Context;
+import com.liferay.prototype.analytics.storage.stubs.Location;
+import com.liferay.prototype.analytics.storage.stubs.Properties;
 import com.liferay.prototype.analytics.storage.stubs.StoredAnalyticsEvent;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -19,7 +29,113 @@ public class DefaultStoredAnalyticsEventFactory
 	public Collection<StoredAnalyticsEvent> create(
 		AnalyticsEvents analyticsEvents) {
 
-		return null;
+		List<Event> events = analyticsEvents.getEvents();
+
+		if (ListUtil.isEmpty(events)) {
+			return Collections.emptyList();
+		}
+
+		Collection<StoredAnalyticsEvent> storedAnalyticsEvents =
+			new ArrayList<>(events.size());
+
+		for (Event event : events) {
+			StoredAnalyticsEvent storedAnalyticsEvent =
+				new StoredAnalyticsEvent();
+
+			storedAnalyticsEvent.setAdditionalInfo(
+				convert(event.getAdditionalInfo()));
+
+			storedAnalyticsEvent.setApplicationId(
+				analyticsEvents.getApplicationId());
+
+			storedAnalyticsEvent.setChannel(analyticsEvents.getChannel());
+
+			storedAnalyticsEvent.setContext(
+				convert(analyticsEvents.getContext()));
+
+			storedAnalyticsEvent.setEvent(event.getEvent());
+			storedAnalyticsEvent.setGroupId(event.getGroupId());
+			storedAnalyticsEvent.setMessageFormat(
+				analyticsEvents.getMessageFormat());
+
+			storedAnalyticsEvent.setProperties(convert(event.getProperties()));
+
+			storedAnalyticsEvent.setTimestamp(event.getTimestamp());
+		}
+
+		return storedAnalyticsEvents;
+	}
+
+	protected AdditionalInfo convert(
+		com.liferay.prototype.analytics.data.binding.stubs.AdditionalInfo
+			additionalInfo) {
+
+		AdditionalInfo storageAdditionalInfo = new AdditionalInfo();
+
+		storageAdditionalInfo.setTime(additionalInfo.getTime());
+
+		Map<String, Object> additionalProperties =
+			additionalInfo.getAdditionalProperties();
+
+		additionalProperties.forEach(
+			storageAdditionalInfo::setAdditionalProperty);
+
+		return storageAdditionalInfo;
+	}
+
+	protected Context convert(
+		com.liferay.prototype.analytics.data.binding.stubs.Context context) {
+
+		Context storedContext = new Context();
+
+		storedContext.setCompanyId(context.getCompanyId());
+		storedContext.setDeviceId(context.getDeviceId());
+		storedContext.setLanguageId(context.getLanguageId());
+		storedContext.setSignedIn(context.getSignedIn());
+		storedContext.setUserId(context.getUserId());
+
+		storedContext.setLocation(convert(context.getLocation()));
+
+		Map<String, Object> additionalProperties =
+			context.getAdditionalProperties();
+
+		additionalProperties.forEach(storedContext::setAdditionalProperty);
+
+		return storedContext;
+	}
+
+	protected Location convert(
+		com.liferay.prototype.analytics.data.binding.stubs.Location location) {
+
+		Location storedLocation = new Location();
+
+		storedLocation.setLatitude(location.getLatitude());
+		storedLocation.setLongitude(location.getLongitude());
+
+		Map<String, Object> additionalProperties =
+			location.getAdditionalProperties();
+
+		additionalProperties.forEach(storedLocation::setAdditionalProperty);
+
+		return storedLocation;
+	}
+
+	protected Properties convert(
+		com.liferay.prototype.analytics.data.binding.stubs.Properties
+			properties) {
+
+		Properties storedProperties = new Properties();
+
+		storedProperties.setElementId(properties.getElementId());
+		storedProperties.setEntityType(properties.getEntityType());
+		storedProperties.setReferrers(properties.getReferrers());
+
+		Map<String, Object> additionalProperties =
+			properties.getAdditionalProperties();
+
+		additionalProperties.forEach(storedProperties::setAdditionalProperty);
+
+		return storedProperties;
 	}
 
 }
--- modules/analytics/analytics-web/src/main/java/com/liferay/prototype/analytics/web/internal/AnalyticsEventsAnalyticMessageProcessor.java
+++ modules/analytics/analytics-web/src/main/java/com/liferay/prototype/analytics/web/internal/AnalyticsEventsAnalyticMessageProcessor.java
@@ -3,16 +3,13 @@ package com.liferay.prototype.analytics.web.internal;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.prototype.analytics.data.binding.stubs.AnalyticsEvents;
-import com.liferay.prototype.analytics.data.binding.stubs.Event;
 import com.liferay.prototype.analytics.processor.AnalyticsMessageProcessor;
 import com.liferay.prototype.analytics.storage.AnalyticsMessageStorage;
 import com.liferay.prototype.analytics.storage.StoredAnalyticsEventFactory;
 import com.liferay.prototype.analytics.storage.stubs.StoredAnalyticsEvent;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -29,18 +26,16 @@ public class AnalyticsEventsAnalyticMessageProcessor
 
 	@Override
 	public void processMessage(AnalyticsEvents analyticsEvents) {
-		List<Event> events = analyticsEvents.getEvents();
+		Collection<StoredAnalyticsEvent> storedAnalyticsEvents =
+			storedAnalyticsEventFactory.create(analyticsEvents);
 
-		if (ListUtil.isEmpty(events)) {
+		if (storedAnalyticsEvents.isEmpty()) {
 			if (_log.isDebugEnabled()) {
 				_log.debug("No events found");
 			}
 
 			return;
 		}
-
-		Collection<StoredAnalyticsEvent> storedAnalyticsEvents =
-			storedAnalyticsEventFactory.create(analyticsEvents);
 
 		try {
 			analyticsMessageStorage.store(storedAnalyticsEvents);
